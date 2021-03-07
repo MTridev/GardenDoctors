22ecf8a8efadd7ad7aead39d3b8cb75c3e8af74a
MTridev <tvmoothoor@student.udm.ac.mu>
2021-03-07 20:21:38 +0400

    Login Page created

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+<div>
+
+    <div class="panel panel-default">
+        <div class="panel-body">
+            Panel content
+        </div>
+        <div class="panel-footer">Panel footer</div>
+    </div>
+
+</div>

73783c8d4a2d30d678f38d2ac47d9f6ba110aff0
MTridev <tvmoothoor@student.udm.ac.mu>
2021-03-11 13:00:10 +0400

    Carousel pictures retrived from Database

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,17 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Garden Doctors</title>
 
-    <!-- jQuery CDN -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-
+    
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
 
 </head>
 <body>
@@ -36,10 +31,20 @@
 <style>
     .customized-login{
         height: 500px;
-        padding-top: 115px;
+        padding-top: 110px;
+    }
+    img.carousel-img{
+        height: 350px !important;
+    }
+    .customized-product{
+        height: 800px;
     }
 
-
+    .slider-text{
+        background-color: #2a9d8f;
+        opacity: 0.8;
+    }
+    
 
 </style>
 
